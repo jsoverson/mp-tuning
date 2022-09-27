12a6fc2b420ae10c7afb5b8a8f48c7842ba4a79c
pinkforest <36498018+pinkforest@users.noreply.github.com>
2022-09-27 12:14:51 +1000

    Add postcard, no_std stuff and other attempts
    Signed-off-by: pinkforest <36498018+pinkforest@users.noreply.github.com>

--- crates/runner/src/main.rs
+++ crates/runner/src/main.rs
@@ -2,18 +2,18 @@ use anyhow::Result;
 use wasmtime::*;
 
 fn main() -> Result<()> {
-  let engine = Engine::default();
-  let file = std::env::args().nth(1).unwrap();
-  let module = Module::from_file(&engine, file)?;
+    let engine = Engine::default();
+    let file = std::env::args().nth(1).unwrap();
+    let module = Module::from_file(&engine, file)?;
 
-  let mut store = Store::new(&engine, ());
+    let mut store = Store::new(&engine, ());
 
-  let imports = [];
-  let instance = Instance::new(&mut store, &module, &imports)?;
+    let imports = [];
+    let instance = Instance::new(&mut store, &module, &imports)?;
 
-  let run = instance.get_typed_func::<i32, i32, _>(&mut store, "run")?;
+    let run = instance.get_typed_func::<i32, i32, _>(&mut store, "run")?;
 
-  let result = run.call(&mut store, 42)?;
-  println!("Result: {}", result);
-  Ok(())
+    let result = run.call(&mut store, 42)?;
+    println!("Result: {}", result);
+    Ok(())
 }
